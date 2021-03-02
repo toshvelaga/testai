f390615caa6426d079e36954a5c00ec375244c66
Santhosh Velaga <toshvelaga@gmail.com>
2021-03-01 16:04:02 -0800

    edited README.md

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Chart } from "react-google-charts";
 
+// React Google Charts Bar Chart: https://react-google-charts.com/bar-chart
+
 function BarChart(props) {
   return (
     <div>
